2d083dac5aaf6eb66d4665d6b55cfe23237eb0a1
Andrew <nowakowski.andrew.m@gmail.com>
2020-08-10 01:39:23 -0700

    adding email sending

--- d3be/mysite/blog/views.py
+++ d3be/mysite/blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import EmailPostForm
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from django.core.mail import send_mail
 
 # Create your views here.
 class PostListView(ListView):
@@ -36,3 +38,23 @@ def post_detail(request, year, month, day, post):
     return render(request,
                  'blog/post/detail.html',
                  {'post': post})
+
+def post_share(request, post_id):
+    # retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == 'POST':
+        # form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'djangotest420@gmail.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
